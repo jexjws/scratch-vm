42437887e4e3d6e312eb50b855bd93ea056a48c8
jexjws <65129712+jexjws@users.noreply.github.com>
2023-11-17 22:54:12 +0800

    Update index.js

--- src/extensions/scratch3_canvas/index.js
+++ src/extensions/scratch3_canvas/index.js
@@ -1,4 +1,4 @@
-const ArgumentType = require('../../extension-support/argument-type');
+/**const ArgumentType = require('../../extension-support/argument-type');
 const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const Clone = require('../../util/clone');
@@ -7,7 +7,7 @@ const formatMessage = require('format-message');
 const MathUtil = require('../../util/math-util');
 const RenderedTarget = require('../../sprites/rendered-target');
 const log = require('../../util/log');
-const StageLayering = require('../../engine/stage-layering');
+const StageLayering = require('../../engine/stage-layering');*/
 
 /**
  * Icon svg to be displayed at the left edge of each extension block, encoded as a data URI.
